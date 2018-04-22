11078b8b2e18134a2f45de54996242ea637573dd
btrejo12 <trejo.brenda95@gmail.com>
2018-04-21 23:44:17 -0500

    Log in and home screen GUI

---
+++ app/src/main/java/nullsource/gds/loginScreen.java
@@ -0,0 +1,13 @@
+package nullsource.gds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
